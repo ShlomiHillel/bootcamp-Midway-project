f3718862473e0c7dad606cfb42392a128ed2fa49
ShlomiHillel <hi.shlomi@gmail.com>
2021-04-03 03:17:15 +0300

    Navbar fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,43 +1,43 @@
-
-
 import React,{useEffect, useState} from 'react';
+import  "./Navbar.css";
 
 const Navbar=() => {
-    const [scrolled,setScrolled]=useState(false);
+    const [scrolled,setScrolled]= useState(false);
   
     const handleScroll=() => {
-      
-      if(window.scrollY > 200 ){
-        setScrolled(true);
-      }
-      else{
-        setScrolled(false);
-      }
-    }
+
+    let v =  window.scrollY
+      v > 200 ?  setScrolled(true): setScrolled(false) ;
+        } 
+   
+
     useEffect(() => {
       window.addEventListener('scroll',handleScroll)
     })
   
-    let x=['navbar'];
-    if(scrolled){
-      x.push('scrolled');
-    }
+    
+    
     return (
-      <div className={x.join(" ")}>
+
+        <div>
+            <input type="button" class="asa" value="home" />
+
+
+        
+            <div className={ scrolled ? 'navbar scrolled' : 'navbar'}>
           {/* <div className="logo">
             <img src={Logo} alt="Logo" title="Logo" />
           </div> */}
   
-          <div className="navigation">
-              <ul>
+              <ul className="navi">
                 <li><a href="#post1">Home</a></li>
                 <li><a href="#post2">Home</a></li>
                 <li><a href="#post3">Home</a></li>
                 <li><a href="#post4">Home</a></li>
               </ul>
-          </div>
+          
   
-      </div>
+      </div></div>
     )
   };
   
--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -11,12 +11,17 @@ const MainPage = () => {
       <header>
         <h1>Welcome To Our Website</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto necessitatibus sunt recusandae a facere totam?</p>
-        <a href="#" className="btn">Find Out More</a>
+        {/* <a href="#" className="btn">Find Out More</a> */}
       </header>
 
       <section>
         <h3>About Our Company</h3>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos repellendus nulla eaque maxime in eveniet aut, cumque reiciendis fugit voluptas saepe distinctio voluptatem excepturi quam rerum tempore debitis possimus? Aliquid adipisci dolorum doloribus voluptate doloremque sed libero dignissimos itaque cumque qui totam, nemo molestiae amet architecto dolore nihil ipsa facilis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos repellendus nulla eaque maxime in eveniet aut, cumque reiciendis fugit voluptas saepe distinctio voluptatem excepturi quam rerum tempore debitis possimus? Aliquid adipisci dolorum doloribus voluptate doloremque sed libero dignissimos itaque cumque qui totam, nemo molestiae amet architecto dolore nihil ipsa facilis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos repellendus nulla eaque maxime in eveniet aut, cumque reiciendis fugit voluptas saepe distinctio voluptatem excepturi quam rerum tempore debitis possimus? Aliquid adipisci dolorum doloribus voluptate doloremque sed libero dignissimos itaque cumque qui totam, nemo molestiae amet architecto dolore nihil ipsa facilis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos repellendus nulla eaque maxime in eveniet aut, cumque reiciendis fugit voluptas saepe distinctio voluptatem excepturi quam rerum tempore debitis possimus? Aliquid adipisci dolorum doloribus voluptate doloremque sed libero dignissimos itaque cumque qui totam, nemo molestiae amet architecto dolore nihil ipsa facilis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos repellendus nulla eaque maxime in eveniet aut, cumque reiciendis fugit voluptas saepe distinctio voluptatem excepturi quam rerum tempore debitis possimus? Aliquid adipisci dolorum doloribus voluptate doloremque sed libero dignissimos itaque cumque qui totam, nemo molestiae amet architecto dolore nihil ipsa facilis?</p>
+        
       </section>
       <br></br>
       
